3fee9f778fd2b33699b8ec5f287c892ae7a81f68
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-23 20:42:34 +0300

    update

---
+++ task-01.js
@@ -0,0 +1,11 @@
+// task-01.js
+
+let name = 'Генератор';
+let price = 1000;
+
+
+console.log(`Обрано «${name}», ціна за одиницю ${price} кредитів`);
+
+price = 2000;
+
+console.log(`Обрано «${name}», ціна за одиницю ${price} кредитів`);
